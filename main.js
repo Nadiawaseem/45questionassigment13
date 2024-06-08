58bc1abf5698db74af8f83572492dd4de57851b3
nadiawaseem <161966596+Nadiawaseem@users.noreply.github.com>
2024-06-08 16:42:42 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+"use strict";
+let names = ["Car", "bike", "bus"];
+let message = 'I would like to own a';
+for (let i = 0; i < names.length; i++) {
+    console.log(message, names[i]);
+}
---
+++ main.ts
@@ -0,0 +1,7 @@
+   let names :string [] = ["Car", "bike", "bus"];
+   let message :string = 'I would like to own a'
+   for (let i = 0; i < names.length; i ++) {
+    console.log(message,names[i]);
+
+   }
+   
